7f23561d3565739b1b8f64ec9b333471d41567ac
SerialGuy <mufaddaldalalmine@gmail.com>
2021-07-26 09:34:32 +0530

    first commit

---
+++ bot.py
@@ -0,0 +1,46 @@
+import discord
+from discord.ext import commands
+import requests
+import json
+from requests.api import get
+client=discord.Client()
+
+def get_quote():
+    response = requests.get('https://zenquotes.io/api/random')   
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q']
+    data = json.loads(response.text)
+    author = data[0]['a']
+    return(quote,author)
+
+k={'thunder':"<@248742722221899778>",'scout':"<@269369856229376000>",'killer':"<@444583334539165697>",
+'rage':"<@337167925519712267>",'taher':"<@713328216634425355>",'serial':"<@250928149494235146>",'tohan':"<@755245777915347055>"}
+c= "abc"
+
+@client.event
+async def on_ready():
+    print("Bot is online as {0.user}".format(client))
+    
+@client.event
+async def on_message(message):
+    if message.content == "!quote":
+        quote,author = get_quote()
+        
+        embedVar = discord.Embed(title="QUOTE", color=0x00ff00)
+        embedVar.add_field(name=(quote), value= (author) , inline=False)
+        await message.channel.send(embed=embedVar)
+
+    if message.content in k :
+        c=k.get(message.content)
+        for a in range(0,100):
+            await message.channel.send(c)
+            
+    
+    
+        
+    if message.content == "!watt":
+        await message.channel.send ("Hiiiiiiiiiiiii")
+
+
+
+client.run('NzM0MzQ4NTg5MTQwMTQ4MjQ0.XxQZaw.jLqNY7x84vhc-L4Agx0jFNWTEYU')
